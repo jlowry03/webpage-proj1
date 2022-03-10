1a849af7735c22e6c9d7fa99144a8c2f74c8771c
Jamie Lowry <jamiedlowry03@gmail.com>
2022-03-10 09:20:22 +0000

    add gitignore and dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil      . ((indent-tabs-mode . t)
+              (fill-column      . 79)
+              (mode             . auto-fill)))
+ (web-mode . ((compile-command  . (format "google-chrome-stable %s"
+                                          buffer-file-name)))))
+ ;;(css-mode . ()))
